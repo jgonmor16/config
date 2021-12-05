864885a6a100cf0f26dcd2ebf620f51126deb60c
Javier Gonzalez Moreno <jgonmor16@gmail.com>
2021-12-05 14:48:07 +0000

    Modify packer autocmd to run PackerSync instead of PackerCompile

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -7,7 +7,7 @@
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
 
