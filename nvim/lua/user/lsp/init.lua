6949ea12cb52df24ea9e4218795462f07dd4a02a
Javier Gonzalez Moreno <jgonmor16@gmail.com>
2022-10-31 07:52:43 +0100

    Add HDL checker to LSP
    Also update "update_capabilities" function to "default_capabilities",
    as it broke after installing nvim v0.8.0.

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -7,5 +7,32 @@ if not status_ok then
     return
 end
 
+-- Only define once
+if not require'lspconfig.configs'.hdl_checker then
+  require'lspconfig.configs'.hdl_checker = {
+    default_config = {
+    cmd = {"hdl_checker", "--lsp", };
+    filetypes = {"vhdl", "verilog", "systemverilog"};
+      root_dir = function(fname)
+        -- will look for the .hdl_checker.config file in parent directory, a
+        -- .git directory, or else use the current directory, in that order.
+        local util = require'lspconfig'.util
+        return util.root_pattern('.hdl_checker.config')(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+      end;
+      settings = {};
+    };
+
+  }
+end
+
+require'lspconfig'.hdl_checker.setup{}
+
+require('nvim-treesitter.configs').setup {
+    ensure_installed = { "norg", --[[ other parsers you would wish to have ]] },
+    highlight = { -- Be sure to enable highlights if you haven't!
+        enable = true,
+    }
+}
+
 require("user.lsp.lsp-installer")
 require("user.lsp.handlers").setup()
