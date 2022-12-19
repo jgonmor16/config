4870a6b6df62d1d57e1cb88bc9da84f65c22a971
Javier Gonzalez Moreno <jgonmor16@gmail.com>
2022-12-19 22:01:46 +0100

    Move plugins configuration to this file
    All the simple plugins are configured directly on this file.
    The complex ones are required here too (by loading their respective
    configuation files).

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,138 +1,57 @@
--------------
--- Plugins --
--------------
-
+-------------------------------------------------------------------------------
+-- Plugins Configuration
 -------------------------------------------------------------------------------
 
-local fn = vim.fn
-local cmd = vim.cmd
 
-cmd("filetype plugin on")
+-------------
+-- Lualine --
+-------------
+require 'user.lualine'
 
--- Automatically install packer
-local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-    }
-    print "Installing packer. Close and reopen nvim..."
-    cmd [[packadd packer.nvim]]
-end
+--------------
+-- Comments --
+--------------
+require 'user.comment'
 
--- Reload nvim whenever this file is saved
-cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-    return
-end
-
-packer.init {
-    display = {
-        open_fn = function()
-            return require("packer.util").float { border = "rounded" }
-        end,
-    },
+---------------
+-- Blankline --
+---------------
+require('indent_blankline').setup {
+  char = '┊',
+  show_trailing_blankline_indent = false,
 }
 
--------------------------------------------------------------------------------
+--------------
+-- Gitsigns --
+--------------
+require('gitsigns').setup {
+  signs = {
+  add = { text = '+' },
+  change = { text = '~' },
+  delete = { text = '_' },
+  topdelete = { text = '‾' },
+  changedelete = { text = '~' },
+  },
+}
 
--- Packer
-return packer.startup(function(use)
+---------------
+-- Telescope --
+---------------
+require 'user.telescope'
 
-    -- Packer
-    use "wbthomason/packer.nvim"
+----------------
+-- Treesitter --
+----------------
+require 'user.treesitter'
 
-    -- Useful lua functions used ny lots of plugins
-    use "nvim-lua/plenary.nvim"
-    -- An implementation of the Popup API from vim in Neovim
-    use "nvim-lua/popup.nvim"
+---------
+-- LSP --
+---------
+require 'user.lsp'
 
-    -- Status line (Lualine) and solarized colorsheme
-    use {
-        "nvim-lualine/lualine.nvim",
-        requires = {
-            "kyazdani42/nvim-web-devicons",
-            opt = true
-        }
-    }
-    use {
-        "svrana/neosolarized.nvim",
-        requires = {
-            "tjdevries/colorbuddy.nvim"
-        }
-    }
+----------------
+-- Completion --
+----------------
+require 'user.completion'
 
-    -- Completion and LSP
-    use "neovim/nvim-lspconfig"             -- LSP
-    use "hrsh7th/nvim-cmp"                  -- cmp autocompletion
-    use "hrsh7th/cmp-buffer"                -- Buffer
-    use "hrsh7th/cmp-path"                  -- Files
-    use "hrsh7th/cmp-nvim-lua"              -- Lua
-    use "hrsh7th/cmp-nvim-lsp"              -- cmp-LSP
-    use "saadparwaiz1/cmp_luasnip"          -- luasnip
-    use "onsails/lspkind-nvim"              -- Cool icons
-    use "williamboman/nvim-lsp-installer"   -- LSP Installer
 
-    -- Treesitter
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate"
-    }
-
-    -- Snip
-    use "L3MON4D3/LuaSnip"
-
-    -- Comments
-    use "numToStr/Comment.nvim"
-
-    -- Telescope
-    use "nvim-telescope/telescope.nvim"
-        -- requires = { {"nvim-lua/plenary.nvim"} }
-
-    -- Neorg
-    use {
-        "nvim-neorg/neorg",
-        tag = "*",                          -- Latest Stable Version
-        run = ":Neorg sync-parsers",
-        requires = "nvim-lua/plenary.nvim"
-    }
-
-    -- Better-Scape
-    -- use {
-    --     "jdhao/better-escape.vim",
-    --     event = "InsertEnter"
-    -- }
-
-    --use "jose-elias-alvarez/nvim-lsp-ts-utils"
-
-    -- Markdown
-    use {
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    }
-
-    -- Git
-    use {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require('gitsigns').setup()
-        end
-    }
-
-    -- Automatically set up config after cloning packer
-    if PACKER_BOOTSTRAP then
-        require("packer").sync()
-    end
-
-end)
