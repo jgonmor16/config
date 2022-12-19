0c15ab51dd3c4982bb4001ce2d907dffe5e1a4bd
Javier Gonzalez Moreno <jgonmor16@gmail.com>
2022-12-19 21:58:44 +0100

    Completely restructure repo
    * Remove files which will not be use in the new conf.
    * Add packer to init.lua (is installed automatically and properly
      configured).
    * Remove other configuration.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,50 +1,206 @@
---------------
+-------------------------------------------------------------------------------
 -- init.lua --
---------------
+-------------------------------------------------------------------------------
 
--- Setup globals
-require "user.global"
+---------------------
+-- Handy variables --
+---------------------
 
--- First load: check if packer is installed, otherwise install it
-if require "user.first_load"() then
-    return
+-- Packer Plugins
+local fn = vim.fn
+local cmd = vim.cmd
+
+cmd('filetype plugin on')
+
+--------------------
+-- Packer Install --
+--------------------
+
+-- Check if packer is installed. Otherwise, install it
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  cmd [[packadd packer.nvim]]
 end
 
-if vim.g.neovide then
-  vim.g.neovide_cursor_trail_legnth = 0
-  vim.g.neovide_cursor_animation_length = 0
-  vim.o.guifont = "Jetbrains Mono"
+--------------------------
+-- Packer Configuration --
+--------------------------
+require('packer').startup(function(use)
+
+  -- Packer (can handle itself)
+  use 'wbthomason/packer.nvim'
+
+  -----------------------
+  -- LSP configuration --
+  -----------------------
+  use {
+    'neovim/nvim-lspconfig',
+    requires = {
+      -- Install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Status updates for LSP
+      'j-hui/fidget.nvim',
+    },
+  }
+
+  --------------------
+  -- Autocompletion --
+  --------------------
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      -- cmp-lsp
+      use 'hrsh7th/cmp-nvim-lsp',
+      -- Snippets
+      use 'L3MON4D3/LuaSnip',
+      use 'saadparwaiz1/cmp_luasnip',
+      -- Buffer and files
+      use 'hrsh7th/cmp-buffer',
+      use 'hrsh7th/cmp-path'
+    },
+  }
+  -- Cool icons for LSP
+  use 'onsails/lspkind-nvim'
+
+  -- Review
+  --use 'hrsh7th/cmp-nvim-lua'        -- Lua
+
+  ----------------
+  -- Treesitter --
+  ----------------
+  -- [INFO]: highlight, edit and navigate code
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      pcall(require('nvim-treesitter.install').update { with_sync = true })
+    end,
+  }
+
+  -- Aditional text objects via treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    after = 'nvim-treesitter',
+  }
+
+  ---------
+  -- Git --
+  ---------
+  use 'lewis6991/gitsigns.nvim'
+
+  -- TODO: check this plugins
+  -- use 'tpope/vim-fugitive'
+  -- use 'tpope/vim-rhubarb'
+
+  ------------------------------------
+  -- Solarized theme and Statusline --
+  ------------------------------------
+  use {
+    'svrana/neosolarized.nvim',
+    requires = {
+      'tjdevries/colorbuddy.nvim'
+    }
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+      opt = true
+    }
+  }
+
+  -- Really cool plugin which automatically detects tabstop and shiftwidth
+  use 'tpope/vim-sleuth'
+
+  ---------------
+  -- Telescope --
+  ---------------
+  -- Fuzzy finder (files, lsp, etc)
+  use {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+  }
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+  -- Only load if `make` is available
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'make',
+    cond = fn.executable 'make' == 1
+  }
+
+  --------------
+  -- Comments --
+  --------------
+  use 'numToStr/Comment.nvim'
+
+  -- Add indentation guides even on blank lines
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  -- Neorg
+  use {
+    'nvim-neorg/neorg',
+    tag = '*',              -- Latest Stable Version
+    run = ':Neorg sync-parsers',
+    requires = 'nvim-lua/plenary.nvim'
+  }
+
+
+  -- TODO: Review this
+  local has_plugins, plugins = pcall(require, 'custom.plugins')
+  if has_plugins then
+    plugins(use)
+  end
+
+  if is_bootstrap then
+    require('packer').sync()
+  end
+
+end)
+
+-- When we are bootstrapping a configuration, it doesn't
+-- make sense to execute the rest of the init.lua.
+--
+-- You'll need to restart nvim, and then it will work.
+if is_bootstrap then
+  print '=================================='
+  print '   Plugins are being installed'
+  print '   Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
 end
 
--- Remap space as leader key
-vim.api.nvim_set_keymap("", "<Space>", "<Nop>", {noremap = true, silent = true})
-vim.g.mapleader = " "
+-- Automatically source and re-compile packer whenever this file is saved
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerSync',
+  group = packer_group,
+  pattern = fn.expand '$MYVIMRC',
+})
+
+------------------------
+-- Nvim Configuration --
+------------------------
 
 -- Disable builtin plugins I do not use
-require "user.disable_buildtin"
+require 'user.disable_buildtin'
+-- Options
+require 'user.option'
+-- Keybindings
+require 'user.mapping'
 
--- Common conf
-vim.cmd("syntax on")
-vim.cmd("filetype indent plugin on")
-vim.cmd("colorscheme solarized")        -- Solarized colorscheme
+---------------------------
+-- Plugins Configuration --
+---------------------------
 
--- Neovim builtin LSP configuration
---require "user.lsp"
-
--- Telescope BTW
---require "user.telescope.setup"
---require "user.telescope.mappings"
-
--- Require all the lua conf files
-require "user.plugins"              -- Plugins
-require "user.option"               -- Options
-require "user.mapping"              -- Keybindings
-require "user.telescope"            -- Telescope configuration
-require "user.completion"           -- cmp configuration
-require "user.lsp"                  -- LSP configuration
-require "user.treesitter"           -- Treesitter configuration
-require "user.comment"              -- Comment configuration
-require "user.luasnippets"          -- Snippets configuration
-require "user.neorg"                -- Neorg configuration
-require "user.lualine"              -- Status Line configuration
+-- Configure plugins added above
+require 'user.plugins'
 
