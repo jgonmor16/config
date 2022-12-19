43774a425c73e76190a00139af097fb08307ed18
Javier Gonzalez Moreno <jgonmor16@gmail.com>
2022-12-19 22:15:28 +0100

    Rework treesitter configuration
    * Add diagnostic keymaps.

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -4,26 +4,71 @@
 
 -- Highlight --
 
-require'nvim-treesitter.configs'.setup {
-  highlight = {
+require('nvim-treesitter.configs').setup {
+  -- Languages to be installed
+  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'help' },
+
+  highlight = { enable = true },
+  indent = { enable = true },
+  incremental_selection = {
     enable = true,
-    custom_captures = {
-      -- Highlight the @foo.bar capture group with the "Identifier" highlight
-      -- group.
-      ["foo.bar"] = "Identifier",
+    keymaps = {
+      init_selection = '<c-space>',
+      node_incremental = '<c-space>',
+      scope_incremental = '<c-s>',
+      node_decremental = '<c-backspace>',
     },
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same
-    -- time. Set this to `true` if you depend on 'syntax' being enabled (like
-    -- for indentation). Using this option may slow down your editor, and you
-    -- may see some duplicate highlights. Instead of true it can also be a list
-    -- of languages
-    additional_vim_regex_highlighting = {'org'},
   },
-    ensure_installed = {'norg'},
+  textobjects = {
+    -- Select
+    select = {
+      enable = true,
+      -- Automatically jump forward to textobj
+      lookahead = true,
+      keymaps = {
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.inner',
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    -- Move
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
+  },
 }
 
--- nvim-solarized-lua --
---vim.g.solarized_italics = 1             -- Default
---vim.g.solarized_visibility = 'normal'   -- Default
---vim.g.solarized_diffmode = 'normal'     -- Default
---vim.g.solarized_statusline = 'normal'   -- Default
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+--vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
