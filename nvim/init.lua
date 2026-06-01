d76fe14189fc72d327260fb21727642e48657488
Javier Gonzalez <jgonmor16@gmail.com>
2026-06-01 21:25:24 +0200

    Create new branch for new minimal config
    Add nvim/init.lua file and include the bare minimun
    configuration:
     - Show relative numbers
     - Prevent wrap and swapfile
     - Set tab size to 4 and always expand them
     - Set leader key to space (" ")
     - Set shortcut to source the config file while editing
     - Set shortcut to write (save) currently opened file

---
+++ nvim/init.lua
@@ -0,0 +1,16 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.swapfile = false
+vim.o.expandtab = true
+
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+
+-- vim.pack.add({
+--         {src = ""},
+-- })
+
