f0b48e445b5b59064c21e4de1280ce66c2ef93d7
Javier Gonzalez <jgonmor16@gmail.com>
2026-07-27 13:51:52 +0200

    Add lsp & Mason, and fix known glitch
    - Fix +q4D73 known glitch
    - Add lspconfig and Mason via vim.pack
    - Install and enable lua-language-server

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,12 +5,22 @@ vim.o.tabstop = 4
 vim.o.swapfile = false
 vim.o.expandtab = true
 
+-- Solve +q4D73 known glitch
+local termfeatures = vim.g.termfeatures or {}
+termfeatures.osc52 = false
+vim.g.termfeatures = termfeatures
+
 vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
--- vim.pack.add({
---         {src = ""},
--- })
+vim.pack.add({
+        { src = "https://github.com/neovim/nvim-lspconfig" },
+        { src = "https://github.com/mason-org/mason.nvim" },
+})
 
+require("mason").setup()
+
+vim.lsp.enable({ "lua_ls" })
