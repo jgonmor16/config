123c9542e9562c0e988b3e284e0ff4420bada683
Javier Gonzalez <jgonmor16@gmail.com>
2026-07-28 17:03:45 +0200

    nvim: add Mason/Treesitter/Solarized, and cleanup
    - Add mason.nvim, nvim-treesitter (main), and solarized.nvim via
      vim.pack
    - Enable Solarized Dark colorscheme (termguicolors + background=dark)
    - Enable Treesitter highlighting for buffers with an installed parser
    - Auto-install required tools via mason-registry instead of manual
      :MasonInstall and enable LSP servers.
    - Add system clipboard integration (clipboard=unnamedplus)
    - Replace fragile <leader>o keystring with a proper Lua function;
      add <leader>x for save-and-quit
    - Remove stray no-arg vim.lsp.completion.enable() call
    - Add section header comments throughout

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,6 @@
+---------------------------------------------------------------------------
+-- Options
+---------------------------------------------------------------------------
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
@@ -9,13 +12,14 @@ vim.o.cursorline = true
 vim.o.colorcolumn = "80"
 vim.o.winborder = "rounded"
 vim.o.scrolloff = 3
+vim.o.clipboard = "unnamedplus"
 
 -- Solve +q4D73 known glitch
 local termfeatures = vim.g.termfeatures or {}
 termfeatures.osc52 = false
 vim.g.termfeatures = termfeatures
 
--- Wrap git commit message bodies at 71 columns (the conventional Git limit)
+-- Wrap git commit message bodies at column 71 (the conventional Git limit)
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "gitcommit",
     callback = function()
@@ -23,19 +27,84 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
+---------------------------------------------------------------------------
+-- Keymaps
+---------------------------------------------------------------------------
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set('n', '<leader>o', function()
+    vim.cmd("update")
+    vim.cmd("source %")
+end, { desc = "Save & reload config" })
+vim.keymap.set('n', '<leader>w', ':write<CR>', { desc = "Save file" })
+vim.keymap.set('n', '<leader>x', ':x<CR>', { desc = "Save and close file" })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format,
+    { desc = "Format buffer via LSP" })
 
+---------------------------------------------------------------------------
+-- Plugins
+---------------------------------------------------------------------------
 vim.pack.add({
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/maxmx03/solarized.nvim" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter",
+      version = "main" },
 })
 
+---------------------------------------------------------------------------
+-- Solarized Theme
+---------------------------------------------------------------------------
+vim.o.termguicolors = true
+vim.o.background = "dark"
+
+require("solarized").setup({})
+vim.cmd.colorscheme("solarized")
+
+---------------------------------------------------------------------------
+-- Treesitter
+---------------------------------------------------------------------------
+require("nvim-treesitter").install({
+    "lua",
+    "vim",
+    "vimdoc",
+    "markdown",
+    "bash",
+    "python",
+    "yaml",
+    "toml",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function()
+        pcall(vim.treesitter.start)
+    end,
+})
+
+---------------------------------------------------------------------------
+-- LSP
+---------------------------------------------------------------------------
 require("mason").setup()
-vim.lsp.enable({ "lua_ls", "basedpyright", "ruff" })
+local mason_registry = require("mason-registry")
+local ensure_installed = {
+    "basedpyright",
+    "lua-language-server",
+    "ruff",
+    "taplo",
+    "tree-sitter-cli",
+    "yaml-language-server",
+}
+
+mason_registry.refresh(function()
+    for _, name in ipairs(ensure_installed) do
+        local ok, pkg = pcall(mason_registry.get_package, name)
+        if ok and not pkg:is_installed() then
+            pkg:install()
+        end
+    end
+end)
+
+vim.lsp.enable(mason_registry)
 
 -- Fix 'vim' global warning
 vim.lsp.config("lua_ls", {
@@ -48,7 +117,9 @@ vim.lsp.config("lua_ls", {
     }
 })
 
+---------------------------------------------------------------------------
 -- Completion
+---------------------------------------------------------------------------
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
@@ -60,7 +131,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
-vim.lsp.completion.enable()
 vim.opt.completeopt:append("noselect")
 
 vim.keymap.set("i", "<C-j>", function()
